994017a993be655b48dbe8e7ea2621d4c179b372
wovago <9787439+wovago@users.noreply.github.com>
2023-03-27 21:54:26 +0200

    rename test key

--- test/test_decrypt.py
+++ test/test_decrypt.py
@@ -8,7 +8,7 @@ SOURCE_PATH = os.path.join(PROJECT_PATH, "src")
 sys.path.append(SOURCE_PATH)
 from dnastore.converter import DNAStore
 
-key_file = os.path.join(PROJECT_PATH, "test", "dna.key")
+key_file = os.path.join(PROJECT_PATH, "test", "test.key")
 
 INPUT = "This is a test."
 ENCODED = DNAStore.encode(INPUT, encrypt=True, key_file=key_file)
